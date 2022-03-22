5b45976c7cf1707fd27536959a015260a2d9b786
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2022-03-22 09:55:41 +0100

    ini_post

--- UitleenApp/product classing/Product.cs
+++ UitleenApp/product classing/Product.cs
@@ -4,6 +4,19 @@ namespace UitleenApp.product_classing
 {
     class Product
     {
-        
+
+        public string Name { get; set; }
+        public string ID { get; set; }
+
+        public string Status { get; set; }
+       
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+
+        // public int CategoryID { get; set; }
+
+
+
     }
 }
