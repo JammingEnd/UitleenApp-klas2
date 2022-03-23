d443883e4ee69ac2e7d157c138fb4b73cba1d06f
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2022-03-23 11:26:23 +0100

    delete cache

--- UitleenApp/Form1.cs
+++ UitleenApp/Form1.cs
@@ -7,19 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UitleenApp.scan_classing;
 
 namespace UitleenApp
 {
     public partial class Dashboard : Form
     {
 
-        public TextBox BarcodeTextBox;
+        private ScanInteractor _scanInteractor;
 
         public Dashboard()
         {
             InitializeComponent();
+
+            _scanInteractor = new ScanInteractor();
         }
 
-     
+
+
+        //dummy method
+        void OnEnter_button()
+        {
+            string codeFromTextBox = "00090000";
+            _scanInteractor.GetScan(codeFromTextBox);
+        }
     }
 }
--- UitleenApp/scan classing/ScanInteractor.cs
+++ UitleenApp/scan classing/ScanInteractor.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 
 namespace UitleenApp.scan_classing
 {
-    class ScanInteractor
+     class ScanInteractor
     {
 
+        
+        public void GetScan(string code)
+        {
+            
+        }
 
 
     }
