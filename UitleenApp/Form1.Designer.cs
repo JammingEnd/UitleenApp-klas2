a11bbdfcfc703748924c699c4901d0c8ffff923e
Martiess <232710ROC@gmail.com>
2022-03-28 14:17:37 +0200

    Test

--- UitleenApp/Form1.Designer.cs
+++ UitleenApp/Form1.Designer.cs
@@ -29,22 +29,74 @@ namespace UitleenApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.TB_Search = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // TB_Search
+            // 
+            this.TB_Search.Location = new System.Drawing.Point(987, 104);
+            this.TB_Search.Multiline = true;
+            this.TB_Search.Name = "TB_Search";
+            this.TB_Search.Size = new System.Drawing.Size(212, 33);
+            this.TB_Search.TabIndex = 0;
+            this.TB_Search.TextChanged += new System.EventHandler(this.TB_Search_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(246, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(721, 556);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Cat"});
+            this.listBox1.Location = new System.Drawing.Point(26, 104);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 524);
+            this.listBox1.TabIndex = 2;
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(1211, 799);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.TB_Search);
             this.Name = "Dashboard";
-            this.Text = "Form1";
+            this.Text = "Uileen Applicatie";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TB_Search;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- UitleenApp/Form1.cs
+++ UitleenApp/Form1.cs
@@ -11,8 +11,10 @@ using UitleenApp.scan_classing;
 
 namespace UitleenApp
 {
+    
     public partial class Dashboard : Form
     {
+<<<<<<< Updated upstream
 
         private ScanInteractor _scanInteractor;
 
@@ -30,6 +32,89 @@ namespace UitleenApp
         {
             string codeFromTextBox = "00090000";
             _scanInteractor.GetScan(codeFromTextBox);
+=======
+        private bool IsCollapsed;
+        public TextBox BarcodeTextBox;
+        public List<string> Catergories = new List<string>();
+        public Dashboard()
+        {
+            InitializeComponent();
+            FocusTextBox();
+        }
+        void AddCatergory()
+        {
+            Catergories.Add("camera");
+            Catergories.Add("lego");
+        }
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (IsCollapsed)
+            {
+                panel1.Height += 10;
+                if (panel1.Size == panel1.MaximumSize)
+                {
+                    timer1.Stop();
+                    IsCollapsed = false;
+
+
+                }
+            }
+            else
+            {
+                panel1.Height -= 10;
+                if (panel1.Size == panel1.MaximumSize)
+                {
+                    timer1.Stop();
+                    IsCollapsed = true;
+
+
+                }
+            }
+        }
+        public void FocusTextBox()
+        {
+            TB_Search.Focus();
+        }
+
+        private void test_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void TB_Search_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_hide_Click(object sender, EventArgs e)
+        {
+            button1.Hide();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+>>>>>>> Stashed changes
         }
     }
+
+
+
 }
